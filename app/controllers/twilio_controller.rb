fad5e57fbdaa1a308f403ea0c1056062b317eafc
Carter Rabasa <carter.rabasa@gmail.com>
2014-02-21 10:33:48 -0800

    pointing to remote MP3 file

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -8,6 +8,7 @@ class TwilioController < ApplicationController
   def voice
   	response = Twilio::TwiML::Response.new do |r|
   	  r.Say 'Hey there. Congrats on integrating Twilio into your Rails 4 app.', :voice => 'alice'
+  	  r.Play 'http://linode.rabasa.com/cantina.mp3'
   	end
 
   	render_twiml response
