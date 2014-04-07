bb97280ad3665bdc537afc73213b57b46299beed
Carter Rabasa <carter.rabasa@gmail.com>
2014-04-07 09:17:35 -0700

    added in code to disable CSRF check for twilio controller

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -5,6 +5,8 @@ class TwilioController < ApplicationController
 
   after_filter :set_header
 
+  skip_before_action :verify_authenticity_token
+
   def voice
   	response = Twilio::TwiML::Response.new do |r|
   	  r.Say 'Hey there. Congrats on integrating Twilio into your Rails 4 app.', :voice => 'alice'
