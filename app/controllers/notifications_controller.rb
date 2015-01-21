3977fe2c873e37a175b67f8c12c1efadd3172a01
Carter Rabasa <carter.rabasa@gmail.com>
2015-01-21 15:53:44 -0800

    cleaned-up

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,12 +1,10 @@
-require 'twilio-ruby'
-
 class NotificationsController < ApplicationController
 
   skip_before_action :verify_authenticity_token
 
   def notify
     client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token
-    message = client.account.messages.create from: 'AAA', to: 'BBB', body: 'Learning to send SMS you are.', media_url: 'http://linode.rabasa.com/yoda.gif', status_callback: request.base_url + '/twilio/status'
+    message = client.messages.create from: 'AAA', to: 'BBB', body: 'Learning to send SMS you are.', media_url: 'http://linode.rabasa.com/yoda.gif', status_callback: request.base_url + '/twilio/status'
     render plain: message.status
   end
 
